784f4a3beda5bdd43d9fef6c8ffa4516ed1c948e
bobo <bisamov@gmail.com>
2019-03-12 13:49:45 -0500

    base.html and closeEvent

--- chat/admin.py
+++ chat/admin.py
@@ -2,18 +2,7 @@ from django.contrib import admin
 from chat.models import *
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-# Register your models here.
 
-# class EventAdmin(admin.ModelAdmin):
-#     '''Chat room'''
-#     list_display = ("creater", "invited_user", "date")
-#
-#     def invited_user(self, obj):
-#         return "\n".join([user.username for user in obj.invited.all()])
-#
-# class ChatAdmin(admin.ModelAdmin):
-#     '''Dialogs'''
-#     list_display = ("room", "user", "text", "date")
 
 class EventResources(resources.ModelResource):
 
@@ -39,6 +28,3 @@ class EventAdmin(ImportExportModelAdmin):
 @admin.register(Chat)
 class ChatAdmin(ImportExportModelAdmin):
     pass
-
-# admin.site.register(Event, EventAdmin)
-#admin.site.register(Chat, ChatAdmin)
--- chat/models.py
+++ chat/models.py
@@ -5,14 +5,19 @@ from django.contrib.auth.models import User
 '''Chat Models such as Event and Chat'''
 class Event(models.Model):
     '''Event Room'''
+    name = models.CharField(max_length=100)
     creater = models.ForeignKey(User, verbose_name="Creater", on_delete=models.CASCADE)
     invited = models.ManyToManyField(User, verbose_name="Participants", related_name="invited_user")
     date = models.DateTimeField("Date Created", auto_now_add=True)
+    is_open = models.BooleanField(default=True, blank=False, null=False)
 
 
     class Meta:
         verbose_name="Event"
         verbose_name_plural = "Events"
+
+    def __str__(self):
+        return self.name
 
 
 class Chat(models.Model):
@@ -26,4 +31,7 @@ class Chat(models.Model):
     class Meta:
         verbose_name="Chat Message"
         verbose_name_plural="Chat Messages"
+
+    def __str__(self):
+        return str(self.room)
 
